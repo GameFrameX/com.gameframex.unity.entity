cc997dfe8dcd08872311a0040e0734c10d0bd36c
AlianBlank <wangfj11@foxmail.com>
2024-04-10 15:50:53 +0800

    [增加]1. 增加防裁剪列表

--- Runtime/GameFrameXEntityCroppingHelper.cs
+++ Runtime/GameFrameXEntityCroppingHelper.cs
@@ -6,7 +6,26 @@ namespace GameFrameX.Entity.Runtime
     {
         private void Start()
         {
-
+            _ = typeof(EntityManager);
+            _ = typeof(HideEntityCompleteEventArgs);
+            _ = typeof(IEntity);
+            _ = typeof(IEntityGroup);
+            _ = typeof(IEntityGroupHelper);
+            _ = typeof(IEntityHelper);
+            _ = typeof(IEntityManager);
+            _ = typeof(ShowEntityDependencyAssetEventArgs);
+            _ = typeof(ShowEntityFailureEventArgs);
+            _ = typeof(ShowEntitySuccessEventArgs);
+            _ = typeof(ShowEntityUpdateEventArgs);
+            _ = typeof(DefaultEntityGroupHelper);
+            _ = typeof(DefaultEntityHelper);
+            _ = typeof(AttachEntityInfo);
+            _ = typeof(Entity);
+            _ = typeof(EntityComponent);
+            _ = typeof(EntityGroupHelperBase);
+            _ = typeof(EntityHelperBase);
+            _ = typeof(EntityLogic);
+            _ = typeof(ShowEntityInfo);
         }
     }
 }
