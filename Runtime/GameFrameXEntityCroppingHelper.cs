22614650b5356b3b3e60dc903562ae341a526df4
AlianBlank <wangfj11@foxmail.com>
2024-04-10 15:45:54 +0800

    [增加]1. 增加实体组件

---
+++ Runtime/GameFrameXEntityCroppingHelper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace GameFrameX.Entity.Runtime
+{
+    public class GameFrameXEntityCroppingHelper : MonoBehaviour
+    {
+        private void Start()
+        {
+
+        }
+    }
+}
